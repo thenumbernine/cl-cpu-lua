068b05981678e445bfb85634a9f75c680068f2f6
thenumbernine <christopher.e.moore@gmail.com>
2022-08-22 01:20:55 +0700

    oops

--- cl-cpu.lua
+++ cl-cpu.lua
@@ -896,7 +896,7 @@ cl.clGetDeviceInfo = makeGetter{
 	[ffi.C.CL_DEVICE_EXTENSIONS] = table{
 		'cl_khr_fp64',
 		--'cl_khr_fp16', -- see the half comments in exec-single.c
-	}:concat' '
+	}:concat' ',
 	[ffi.C.CL_DEVICE_PLATFORM] = {
 		type = 'cl_platform_id',
 		getArray = function(paramSize, resultPtr, sizePtr)
