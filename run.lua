a017e86239d86629a2d397c30bae5596a187d961
thenumbernine <christopher.e.moore@gmail.com>
2022-09-16 10:20:11 +0700

    updating to new ext.file

--- run.lua
+++ run.lua
@@ -3,7 +3,7 @@
 local execfn = arg[0]
 
 require 'cl-cpu.setup'
-require 'ffi.OpenCL'.pathToCLCPU = require 'ext.io'.getfiledir(execfn)
+require 'ffi.OpenCL'.pathToCLCPU = require 'ext.file'(execfn):getdir()
 
 local filename = ...
 loadfile(filename)(select(2, ...))
